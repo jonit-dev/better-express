dafc2094deeadfefb703230243c0b046ae744eae
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-11-10 10:51:22 -0800

    inversify merge:

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import 'reflect-metadata'
+import 'reflect-metadata';
+
 import bodyParser from 'body-parser';
-import cookieSession from 'cookie-session';
-import express, { Application } from 'express';
 import { InversifyExpressServer } from 'inversify-express-utils';
+
 import { container } from './config/inversify';
 
-import { loginRouter } from './routes/login.routes';
-
-const server = new InversifyExpressServer(container)
+const server = new InversifyExpressServer(container);
 
 server.setConfig((app) => {
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }))
-  app.use(bodyParser.json())
-})
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(bodyParser.json());
+});
 
 let app = server.build();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
- app.listen(port, () => {
-   console.log(`Server listening on port ${port}`)
- })
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- src/models/user/user.controller.ts
+++ src/models/user/user.controller.ts
@@ -1,17 +1,16 @@
+import { Request, Response } from 'express';
+import { inject } from 'inversify';
+import { controller, httpGet, interfaces } from 'inversify-express-utils';
 
-import { interfaces, controller, httpGet, httpPost, httpDelete, request, queryParam, response, requestParam } from "inversify-express-utils";
-import { injectable, inject } from "inversify";
-import { UserService } from "./user.service";
-import {Request, Response, NextFunction} from 'express'
-import { IUser } from "../../types/user.types";
+import { IUser } from '../../types/user.types';
+import { UserService } from './user.service';
 
 @controller("/users")
 export class UserController implements interfaces.Controller {
-    constructor(@inject("UserService") private userService: UserService) {}
+  constructor(@inject("UserService") private userService: UserService) {}
 
-    @httpGet("/")
-    private index(req: Request, res: Response): IUser[] {
-        return this.userService.getUsers();
-    }
-
+  @httpGet("/")
+  private index(req: Request, res: Response): IUser[] {
+    return this.userService.getUsers();
+  }
 }
